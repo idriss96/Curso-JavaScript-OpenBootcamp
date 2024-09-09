40ed18efbdeb52b1a95d1b6db7b64bcefdb9ab67
idriss96 <idrissatik1996@gmail.com>
2024-09-09 16:37:14 +0200

    tipos primitivos

---
+++ tipos-primitivos.js
@@ -0,0 +1,27 @@
+// tipos primitivos
+
+// number
+4;
+0;
+
+// string
+"hola mundo";
+'hola mundo';
+`hola mundo`;
+
+// booleanos
+true;
+false;
+
+// nulo y undefined
+null;
+undefined;
+
+// null, undefined, false, 0 ==> todas son Falsy
+console.log(null === undefined);
+
+if (true) {
+  console.log('cumple');
+} else {
+  console.log('no cumple');
+}
